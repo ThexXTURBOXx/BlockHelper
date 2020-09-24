5d7bac91a443202681f32ca335dc95dc5d4c6b99
Nico Mexis <nico.mexis@kabelmail.de>
2020-09-24 10:38:36 +0200

    Fix IC² power being off

--- minecraft/de/thexxturboxx/blockhelper/integration/Ic2Integration.java
+++ minecraft/de/thexxturboxx/blockhelper/integration/Ic2Integration.java
@@ -2,6 +2,7 @@ package de.thexxturboxx.blockhelper.integration;
 
 import de.thexxturboxx.blockhelper.InfoHolder;
 import de.thexxturboxx.blockhelper.api.BlockHelperInfoProvider;
+import ic2.api.IEnergyStorage;
 import ic2.common.TileEntityElecMachine;
 import ic2.common.TileEntityElectricBlock;
 import ic2.common.TileEntityMatter;
@@ -12,15 +13,22 @@ public class Ic2Integration extends BlockHelperInfoProvider {
     @Override
     public void addInformation(TileEntity te, int id, int meta, InfoHolder info) {
         if (iof(te, "ic2.common.TileEntityElecMachine")) {
-            info.add(((TileEntityElecMachine) te).energy + " EU / "
-                    + ((TileEntityElecMachine) te).maxEnergy + " EU");
+            TileEntityElecMachine teem = ((TileEntityElecMachine) te);
+            int input = teem.maxInput;
+            int energy = teem.energy;
+            int maxEnergy = teem.maxEnergy;
+            int newEnergy = Math.min(maxEnergy, (maxEnergy * energy) / (maxEnergy - input));
+            info.add(newEnergy + " EU / " + maxEnergy + " EU");
             if (iof(te, "ic2.common.TileEntityMatter")) {
                 info.add("Progress: " + ((TileEntityMatter) te).getProgressAsString());
             }
         }
-        if (iof(te, "ic2.common.TileEntityElectricBlock")) {
-            info.add(((TileEntityElectricBlock) te).energy + " EU / "
-                    + ((TileEntityElectricBlock) te).maxStorage + " EU");
+        if (iof(te, "ic2.api.IEnergyStorage")) {
+            IEnergyStorage storage = ((IEnergyStorage) te);
+            info.add(storage.getStored() + " EU / " + storage.getCapacity() + " EU");
+        } else if (iof(te, "ic2.common.TileEntityElectricBlock")) {
+            TileEntityElectricBlock teeb = (TileEntityElectricBlock) te;
+            info.add(teeb.energy + " EU / " + teeb.maxStorage + " EU");
         }
     }
 
