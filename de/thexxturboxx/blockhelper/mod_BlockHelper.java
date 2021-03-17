bb8e44620493eb575952afbc86ca234e3a6515c9
Nico Mexis <nico.mexis@kabelmail.de>
2021-03-17 10:43:32 +0100

    Show additional info in grey

--- de/thexxturboxx/blockhelper/mod_BlockHelper.java
+++ de/thexxturboxx/blockhelper/mod_BlockHelper.java
@@ -203,7 +203,7 @@ public class mod_BlockHelper extends BaseMod implements IPacketHandler {
                     addInfo(name);
                     addInfo(itemId);
                     addInfo("§o" + ct.replaceAll("§.", ""), 0x000000ff);
-                    addInfo(packetInfos);
+                    addAdditionalInfo(packetInfos);
                     addInfo((harvestable ? "§a✔" : "§4✘") + " §r" + harvest);
                     drawInfo(xy, mc);
                     break;
@@ -218,7 +218,7 @@ public class mod_BlockHelper extends BaseMod implements IPacketHandler {
                         nameEntity = "§4" + nameEntity;
                     }
                     addInfo(nameEntity);
-                    addInfo(packetInfos);
+                    addAdditionalInfo(packetInfos);
                     drawInfo(xy, mc);
                     break;
                 default:
@@ -272,9 +272,9 @@ public class mod_BlockHelper extends BaseMod implements IPacketHandler {
     private static List<String> packetInfos = new ArrayList<String>();
     private static final List<FormatString> infos = new ArrayList<FormatString>();
 
-    private void addInfo(List<String> info) {
+    private void addAdditionalInfo(List<String> info) {
         for (String s : info) {
-            addInfo(s);
+            addInfo("§7" + s);
         }
     }
 
