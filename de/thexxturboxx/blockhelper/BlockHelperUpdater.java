ebae502487339af02afd8d59f6bda693b9b48a46
Nico Mexis <nico.mexis@kabelmail.de>
2021-05-10 10:35:03 +0200

    Updated Update Checker System

--- de/thexxturboxx/blockhelper/BlockHelperUpdater.java
+++ de/thexxturboxx/blockhelper/BlockHelperUpdater.java
@@ -5,14 +5,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import net.minecraft.client.Minecraft;
 
 public class BlockHelperUpdater implements Runnable {
 
     private static final String JSON_URL = "https://raw.githubusercontent.com/"
-            + "ThexXTURBOXx/UpdateJSONs/master/blockhelper.json";
+            + "ThexXTURBOXx/UpdateJSONs/master/block-helper.csv";
     private static boolean isLatestVersion = true;
     private static String latestVersion = "";
 
@@ -21,35 +19,16 @@ public class BlockHelperUpdater implements Runnable {
      */
     @Override
     public void run() {
-        InputStream in = null;
         try {
-            in = new URL(JSON_URL).openStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-            mod_BlockHelper.LOGGER.warning("Update check for " + mod_BlockHelper.NAME + " failed.");
-        }
-        if (in != null) {
-            try {
-                for (String s : readLines(in)) {
-                    if (s.contains("modVersion")) {
-                        s = s.substring(0, s.indexOf("|"));
-                        s = s.replace("modVersion", "");
-                        s = s.replace("\"", "");
-                        s = s.replace(",", "");
-                        s = s.replace("	", "");
-                        s = s.replace(":", "");
-                        latestVersion = s;
-                    }
-                }
-                if (!latestVersion.equalsIgnoreCase(mod_BlockHelper.VERSION)) {
-                    mod_BlockHelper.LOGGER.info("Newer version of " + mod_BlockHelper.NAME + " available: " + latestVersion);
-                } else {
-                    mod_BlockHelper.LOGGER.info("Yay! You have the newest version of " + mod_BlockHelper.NAME + " :)");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-                mod_BlockHelper.LOGGER.warning("Update check for " + mod_BlockHelper.NAME + " failed.");
+            latestVersion = getLatestModVersion(new URL(JSON_URL).openStream());
+            if (!mod_BlockHelper.VERSION.equals(latestVersion)) {
+                mod_BlockHelper.LOGGER.info("Newer version of " + mod_BlockHelper.NAME + " available: " + latestVersion);
+            } else {
+                mod_BlockHelper.LOGGER.info("Yay! You have the newest version of " + mod_BlockHelper.NAME + " :)");
             }
+        } catch (Throwable t) {
+            t.printStackTrace();
+            mod_BlockHelper.LOGGER.warning("Update check for " + mod_BlockHelper.NAME + " failed.");
         }
         isLatestVersion = mod_BlockHelper.VERSION.equals(latestVersion);
     }
@@ -71,16 +50,24 @@ public class BlockHelperUpdater implements Runnable {
         return latestVersion;
     }
 
-    private static List<String> readLines(InputStream input) throws IOException {
-        InputStreamReader reader = new InputStreamReader(input);
-        BufferedReader readers = new BufferedReader(reader);
-        List<String> list = new ArrayList<String>();
-        String line = readers.readLine();
-        while (line != null) {
-            list.add(line);
-            line = readers.readLine();
+    private static String getLatestModVersion(InputStream is) throws IOException {
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        try {
+            String line = br.readLine();
+            while (line != null) {
+                String[] split = line.split(",", 2);
+                if (mod_BlockHelper.MC_VERSION.equals(split[0])) {
+                    return split[1];
+                }
+                line = br.readLine();
+            }
+            throw new IllegalArgumentException("Version not found.");
+        } finally {
+            is.close();
+            isr.close();
+            br.close();
         }
-        return list;
     }
 
     static void notifyUpdater(Minecraft mc) {
