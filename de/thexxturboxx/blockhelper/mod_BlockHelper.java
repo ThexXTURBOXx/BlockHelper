397fc0f05ceb27f3b8c6ffbecc15052fe588fcee
Nico Mexis <nico.mexis@kabelmail.de>
2021-05-10 10:35:16 +0200

    Updated Update Checker System

--- de/thexxturboxx/blockhelper/mod_BlockHelper.java
+++ de/thexxturboxx/blockhelper/mod_BlockHelper.java
@@ -46,7 +46,8 @@ public class mod_BlockHelper extends BaseMod implements IPacketHandler {
     private static final String PACKAGE = "de.thexxturboxx.blockhelper.";
     private static final String MOD_ID = "mod_BlockHelper";
     static final String NAME = "Block Helper";
-    static final String VERSION = "0.9";
+    static final String VERSION = "1.0.0";
+    static final String MC_VERSION = "1.5.2";
     static final String CHANNEL = "BlockHelperInfo";
 
     public static final Logger LOGGER = Logger.getLogger(NAME);
