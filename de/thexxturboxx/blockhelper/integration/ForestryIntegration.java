f9ef87b9a62fc9501a8d79e4760cb4ca046aa83b
Nico Mexis <nico.mexis@kabelmail.de>
2021-10-06 21:44:17 +0200

    Support Forestry

---
+++ de/thexxturboxx/blockhelper/integration/ForestryIntegration.java
@@ -0,0 +1,22 @@
+package de.thexxturboxx.blockhelper.integration;
+
+import de.thexxturboxx.blockhelper.api.BlockHelperBlockState;
+import de.thexxturboxx.blockhelper.api.BlockHelperInfoProvider;
+import de.thexxturboxx.blockhelper.api.InfoHolder;
+import forestry.core.gadgets.TileMachine;
+import forestry.energy.gadgets.MachineGenerator;
+
+public class ForestryIntegration extends BlockHelperInfoProvider {
+
+    @Override
+    public void addInformation(BlockHelperBlockState state, InfoHolder info) {
+        if (iof(state.te, "forestry.core.gadgets.TileMachine")) {
+            TileMachine machine = (TileMachine) state.te;
+            if (iof(machine.machine, "forestry.energy.gadgets.MachineGenerator")) {
+                MachineGenerator generator = (MachineGenerator) machine.machine;
+                info.add(generator.energyStored + " EU / " + generator.energyMax + " EU");
+            }
+        }
+    }
+
+}
