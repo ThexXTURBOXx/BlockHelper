109837906cf3a626ea711252d83ac38d8cd0c180
Nico Mexis <nico.mexis@kabelmail.de>
2021-10-07 19:59:35 +0200

    Support even more AE2 stuff

--- de/thexxturboxx/blockhelper/integration/AppEngIntegration.java
+++ de/thexxturboxx/blockhelper/integration/AppEngIntegration.java
@@ -1,5 +1,6 @@
 package de.thexxturboxx.blockhelper.integration;
 
+import appeng.api.me.tiles.IMEPowerStorage;
 import appeng.me.basetiles.TilePoweredBase;
 import de.thexxturboxx.blockhelper.api.BlockHelperBlockState;
 import de.thexxturboxx.blockhelper.api.BlockHelperInfoProvider;
@@ -12,7 +13,14 @@ public class AppEngIntegration extends BlockHelperInfoProvider {
         if (iof(state.te, "appeng.me.basetiles.TilePoweredBase")) {
             info.add(((TilePoweredBase) state.te).storedPower + " AE / "
                     + ((TilePoweredBase) state.te).maxStoredPower + " AE");
+        } else if (iof(state.te, "appeng.api.me.tiles.IMEPowerStorage")) {
+            IMEPowerStorage ps = (IMEPowerStorage) state.te;
+            double stored = ps.getMECurrentPower();
+            double max = ps.getMEMaxPower();
+            if (max != 0) {
+                info.add(stored + " AE / " + max + " AE");
         }
+    }
     }
 
 }
