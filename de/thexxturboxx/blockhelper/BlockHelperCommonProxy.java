91c7b77df95bda1b1e717e3e32e3da94c0f3f2d0
Nico Mexis <nico.mexis@kabelmail.de>
2021-10-20 19:25:15 +0200

    Cleanup proxies

--- de/thexxturboxx/blockhelper/BlockHelperCommonProxy.java
+++ de/thexxturboxx/blockhelper/BlockHelperCommonProxy.java
@@ -2,21 +2,10 @@ package de.thexxturboxx.blockhelper;
 
 import cpw.mods.fml.common.network.NetworkRegistry;
 import de.thexxturboxx.blockhelper.integration.IntegrationRegistrar;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.src.ModLoader;
 import net.minecraft.src.mod_BlockHelper;
-import net.minecraft.world.World;
-import net.minecraftforge.common.DimensionManager;
 
 public class BlockHelperCommonProxy {
-
-    public EntityPlayer getPlayer() {
-        return null;
-    }
-
-    public World getWorld() {
-        return DimensionManager.getWorld(0);
-    }
 
     public void load(mod_BlockHelper instance) {
         mod_BlockHelper.isClient = false;
