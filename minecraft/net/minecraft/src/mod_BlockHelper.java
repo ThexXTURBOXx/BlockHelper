da38748ad9b7a44dda2815c75450b437f0658e8b
Nico Mexis <nico.mexis@kabelmail.de>
2022-04-02 22:03:55 +0200

    Update version number

--- minecraft/net/minecraft/src/mod_BlockHelper.java
+++ minecraft/net/minecraft/src/mod_BlockHelper.java
@@ -28,7 +28,7 @@ public class mod_BlockHelper extends NetworkMod implements IConnectionHandler, I
 
     public static final String MOD_ID = "mod_BlockHelper";
     public static final String NAME = "Block Helper";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
     public static final String MC_VERSION = "1.2.5";
     public static final String CHANNEL = "BlockHelperInfo";
     public static final String CHANNEL_SSP = "BlockHelperInfoSSP";
