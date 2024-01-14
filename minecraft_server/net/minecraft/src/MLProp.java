207c9baf8ca0a214969fb017a4481c3c6845fd33
Nico Mexis <nico.mexis@kabelmail.de>
2024-01-14 16:32:56 +0100

    Include MLProp fix in b1.6.6 server

---
+++ minecraft_server/net/minecraft/src/MLProp.java
@@ -0,0 +1,33 @@
+// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) braces deadcode
+
+package net.minecraft.src;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({java.lang.annotation.ElementType.FIELD})
+public @interface MLProp {
+    /**
+     * Overrides the field name for property key.
+     */
+    String name() default "";
+
+    /**
+     * Adds additional help to top of configuration file.
+     */
+    String info() default "";
+
+    /**
+     * Minimum value allowed if field is a number.
+     */
+    double min() default Double.NEGATIVE_INFINITY;
+
+    /**
+     * Maximum value allowed if field is a number.
+     */
+    double max() default Double.POSITIVE_INFINITY;
+}
--- minecraft_server/net/minecraft/src/mod_BlockHelper.java
+++ minecraft_server/net/minecraft/src/mod_BlockHelper.java
@@ -7,14 +7,22 @@ import de.thexxturboxx.blockhelper.PacketCoder;
 import de.thexxturboxx.blockhelper.PacketInfo;
 import de.thexxturboxx.blockhelper.api.BlockHelperBlockState;
 import de.thexxturboxx.blockhelper.api.BlockHelperEntityState;
+import de.thexxturboxx.blockhelper.api.BlockHelperInfoProvider;
 import de.thexxturboxx.blockhelper.api.BlockHelperModSupport;
 import de.thexxturboxx.blockhelper.i18n.I18n;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class mod_BlockHelper extends BaseModMp {
@@ -45,6 +53,24 @@ public class mod_BlockHelper extends BaseModMp {
     public static String vanillaIntegrationStr = "true";
     // Configuration entries end
 
+    static {
+        Method m = BlockHelperInfoProvider.getDeclaredMethod(ModLoader.class, "setupProperties");
+        if (m == null) {
+            try {
+                setupProperties(mod_BlockHelper.class);
+            } catch (IllegalAccessException e) {
+                LOGGER.throwing(NAME, "setupProperties", e);
+                ModLoader.ThrowException(NAME, e);
+            } catch (IOException e) {
+                LOGGER.throwing(NAME, "setupProperties", e);
+                ModLoader.ThrowException(NAME, e);
+            } catch (URISyntaxException e) {
+                LOGGER.throwing(NAME, "setupProperties", e);
+                ModLoader.ThrowException(NAME, e);
+            }
+        }
+    }
+
     public static String getModId() {
         return MOD_ID;
     }
@@ -56,6 +82,7 @@ public class mod_BlockHelper extends BaseModMp {
 
     @Override
     public void ModsLoaded() {
+        super.ModsLoaded();
         INSTANCE = this;
         proxy = new BlockHelperCommonProxy();
         proxy.load(this);
@@ -167,6 +194,86 @@ public class mod_BlockHelper extends BaseModMp {
         String var2 = stack.getItem().getItemName();
         return StringTranslate.getInstance().translateKey(
                 var2 == null ? "" : (StatCollector.translateToLocal(var2) + ".name")).trim();
+    }
+
+    private static void setupProperties(Class<? extends BaseMod> class1)
+            throws IllegalArgumentException, IllegalAccessException, IOException, SecurityException,
+            URISyntaxException {
+        String s0 = ModLoader.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+        s0 = s0.substring(0, s0.lastIndexOf('/'));
+        File cfgdir = new File(s0, "/config/");
+        cfgdir.mkdir();
+        Properties properties = new Properties();
+        File file = new File(cfgdir, class1.getName() + ".cfg");
+        if (file.exists() && file.canRead()) {
+            properties.load(new FileInputStream(file));
+        }
+        StringBuilder stringbuilder = new StringBuilder();
+        Field[] afield;
+        int j = (afield = class1.getFields()).length;
+        for (int i = 0; i < j; i++) {
+            Field field = afield[i];
+            if ((field.getModifiers() & 8) == 0 || !field.isAnnotationPresent(MLProp.class)) {
+                continue;
+            }
+            Class<?> class2 = field.getType();
+            MLProp mlprop = (MLProp) field.getAnnotation(MLProp.class);
+            String s = !mlprop.name().isEmpty() ? mlprop.name() : field.getName();
+            Object obj = field.get(null);
+            StringBuilder stringbuilder1 = new StringBuilder();
+            if (mlprop.min() != Double.NEGATIVE_INFINITY) {
+                stringbuilder1.append(String.format(",>=%.1f", mlprop.min()));
+            }
+            if (mlprop.max() != Double.POSITIVE_INFINITY) {
+                stringbuilder1.append(String.format(",<=%.1f", mlprop.max()));
+            }
+            StringBuilder stringbuilder2 = new StringBuilder();
+            if (!mlprop.info().isEmpty()) {
+                stringbuilder2.append(" -- ");
+                stringbuilder2.append(mlprop.info());
+            }
+            stringbuilder.append(String.format("%s (%s:%s%s)%s\n", s, class2.getName(), obj, stringbuilder1,
+                    stringbuilder2));
+            if (properties.containsKey(s)) {
+                String s1 = properties.getProperty(s);
+                Object obj1 = null;
+                if (class2.isAssignableFrom(String.class)) {
+                    obj1 = s1;
+                } else if (class2.isAssignableFrom(Integer.TYPE)) {
+                    obj1 = Integer.parseInt(s1);
+                } else if (class2.isAssignableFrom(Short.TYPE)) {
+                    obj1 = Short.parseShort(s1);
+                } else if (class2.isAssignableFrom(Byte.TYPE)) {
+                    obj1 = Byte.parseByte(s1);
+                } else if (class2.isAssignableFrom(Boolean.TYPE)) {
+                    obj1 = Boolean.parseBoolean(s1);
+                } else if (class2.isAssignableFrom(Float.TYPE)) {
+                    obj1 = Float.parseFloat(s1);
+                } else if (class2.isAssignableFrom(Double.TYPE)) {
+                    obj1 = Double.parseDouble(s1);
+                }
+                if (obj1 == null) {
+                    continue;
+                }
+                if (obj1 instanceof Number) {
+                    double d = ((Number) obj1).doubleValue();
+                    if (mlprop.min() != Double.NEGATIVE_INFINITY && d < mlprop.min() || mlprop.max() != Double.POSITIVE_INFINITY && d > mlprop.max()) {
+                        continue;
+                    }
+                }
+                LOGGER.finer(s + " set to " + obj1);
+                if (!obj1.equals(obj)) {
+                    field.set(null, obj1);
+                }
+            } else {
+                LOGGER.finer(s + " not in config, using default: " + obj);
+                properties.setProperty(s, obj.toString());
+            }
+        }
+
+        if (!properties.isEmpty() && (file.exists() || file.createNewFile()) && file.canWrite()) {
+            properties.store(new FileOutputStream(file), stringbuilder.toString());
+        }
     }
 
 }
