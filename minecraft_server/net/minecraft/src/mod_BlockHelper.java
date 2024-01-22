49f15cc9d7958b9dc3b8940a05afa73a6ed65ebd
Nico Mexis <nico.mexis@kabelmail.de>
2024-01-22 23:17:27 +0100

    Bump version and update README

--- minecraft_server/net/minecraft/src/mod_BlockHelper.java
+++ minecraft_server/net/minecraft/src/mod_BlockHelper.java
@@ -26,7 +26,7 @@ public class mod_BlockHelper extends NetworkMod implements IConnectionHandler, I
 
     public static final String MOD_ID = "mod_BlockHelper";
     public static final String NAME = "Block Helper";
-    public static final String VERSION = "1.2.0-pre2";
+    public static final String VERSION = "1.2.0-pre3";
     public static final String MC_VERSION = "1.2.5";
     public static final String CHANNEL = "BlockHelperInfo";
     public static mod_BlockHelper INSTANCE;
