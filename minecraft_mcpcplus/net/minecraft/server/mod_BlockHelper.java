417f480e2e2664fe743ace0954dd7ec2ec9e768a
Nico Mexis <nico.mexis@kabelmail.de>
2024-01-22 23:17:07 +0100

    Bump version and update README

--- minecraft_mcpcplus/net/minecraft/server/mod_BlockHelper.java
+++ minecraft_mcpcplus/net/minecraft/server/mod_BlockHelper.java
@@ -21,7 +21,7 @@ public class mod_BlockHelper extends BaseModMp {
 
     public static final String MOD_ID = "mod_BlockHelper";
     public static final String NAME = "Block Helper";
-    public static final String VERSION = "1.2.0-pre2";
+    public static final String VERSION = "1.2.0-pre3";
     public static final String MC_VERSION = "1.0";
     public static final String CHANNEL = "BlockHelperInfo";
     public static mod_BlockHelper INSTANCE;
