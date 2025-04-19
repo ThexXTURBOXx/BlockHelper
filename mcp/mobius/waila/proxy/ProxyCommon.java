44a550309ba3b02c2ae6ec177506bb3b827fdba3
Nico Mexis <nico.mexis@kabelmail.de>
2025-04-19 13:14:48 +0200

    Remove RedPower for now

--- mcp/mobius/waila/proxy/ProxyCommon.java
+++ mcp/mobius/waila/proxy/ProxyCommon.java
@@ -15,7 +15,6 @@ import mcp.mobius.waila.addons.forge.ForgePlugin;
 import mcp.mobius.waila.addons.harvestcraft.HarvestcraftPlugin;
 import mcp.mobius.waila.addons.ic2.IC2Plugin;
 import mcp.mobius.waila.addons.natura.NaturaPlugin;
-import mcp.mobius.waila.addons.projectred.ProjectRedPlugin;
 import mcp.mobius.waila.addons.thermalexpansion.ThermalExpansionPlugin;
 import mcp.mobius.waila.addons.vanilla.VanillaPlugin;
 import mcp.mobius.waila.api.IRegistrar;
@@ -48,7 +47,6 @@ public class ProxyCommon {
         registerPlugin(HarvestcraftPlugin.INSTANCE);
         registerPlugin(IC2Plugin.INSTANCE);
         registerPlugin(NaturaPlugin.INSTANCE);
-        registerPlugin(ProjectRedPlugin.INSTANCE);
         registerPlugin(ThermalExpansionPlugin.INSTANCE);
     }
 
