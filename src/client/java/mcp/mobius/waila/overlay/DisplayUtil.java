8308649208d7e70cbf1da47433ef68938e744f4a
Nico Mexis <nico.mexis@kabelmail.de>
2025-06-04 10:12:42 +0200

    Add some new APIs and translations

--- src/client/java/mcp/mobius/waila/overlay/DisplayUtil.java
+++ src/client/java/mcp/mobius/waila/overlay/DisplayUtil.java
@@ -152,7 +152,7 @@ public final class DisplayUtil {
     }
 
     @SuppressWarnings("unchecked")
-    public static List<String> itemDisplayNameMultiline(ItemStack itemstack) {
+    public static List<String> itemDisplayNameMultilineUnformatted(ItemStack itemstack) {
         List<String> namelist = new ArrayList<String>();
         try {
             String name = I18n.translate(itemstack.getItem().getItemNameIS(itemstack) + ".name");
@@ -167,6 +167,16 @@ public final class DisplayUtil {
         if (namelist.get(0) == null || namelist.get(0).isEmpty())
             namelist.set(0, "Unnamed");
 
+        return namelist;
+    }
+
+    public static String itemDisplayNameShortUnformatted(ItemStack itemstack) {
+        return itemDisplayNameMultilineUnformatted(itemstack).get(0);
+    }
+
+    public static List<String> itemDisplayNameMultiline(ItemStack itemstack) {
+        List<String> namelist = itemDisplayNameMultilineUnformatted(itemstack);
+
         namelist.set(0, WHITE + namelist.get(0));
         for (int i = 1; i < namelist.size(); i++)
             namelist.set(i, GRAY + namelist.get(i));
@@ -175,8 +185,7 @@ public final class DisplayUtil {
     }
 
     public static String itemDisplayNameShort(ItemStack itemstack) {
-        List<String> list = itemDisplayNameMultiline(itemstack);
-        return list.get(0);
+        return itemDisplayNameMultiline(itemstack).get(0);
     }
 
     public static void renderIcon(int x, int y, int sx, int sy, IconUI icon) {
