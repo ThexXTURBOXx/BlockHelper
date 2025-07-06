1365f744e61438f2ff7bd341ec4ba00eed2e344d
Nico Mexis <nico.mexis@kabelmail.de>
2025-07-06 20:59:50 +0200

    Backport changes from 1.0

--- src/client/java/mcp/mobius/waila/proxy/ProxyCommon.java
+++ src/client/java/mcp/mobius/waila/proxy/ProxyCommon.java
@@ -15,12 +15,20 @@ import mcp.mobius.waila.addons.thermalexpansion.ThermalExpansionPlugin;
 import mcp.mobius.waila.addons.vanilla.VanillaPlugin;
 import mcp.mobius.waila.api.IRegistrar;
 import mcp.mobius.waila.api.IWailaPlugin;
+import mcp.mobius.waila.api.event.WailaEventRegistrar;
+import mcp.mobius.waila.api.event.WailaRegisterEvent;
 
 public class ProxyCommon {
 
+    private final List<IWailaPlugin> corePlugins = new ArrayList<IWailaPlugin>();
     private final List<IWailaPlugin> plugins = new ArrayList<IWailaPlugin>();
 
     public ProxyCommon() {
+    }
+
+    // Do NOT use this outside of BlockHelper!
+    private void registerCorePlugin(IWailaPlugin plugin) {
+        corePlugins.add(plugin);
     }
 
     public void registerPlugin(IWailaPlugin plugin) {
@@ -28,8 +36,6 @@ public class ProxyCommon {
     }
 
     public void prepare() {
-        registerPlugin(CorePlugin.INSTANCE);
-        registerPlugin(VanillaPlugin.INSTANCE);
         registerPlugin(AdvMachinesASPlugin.INSTANCE);
         registerPlugin(AdvSolarsPlugin.INSTANCE);
         registerPlugin(EEPlugin.INSTANCE);
@@ -42,12 +48,21 @@ public class ProxyCommon {
     }
 
     public void registerCorePlugins(IRegistrar registrar) {
+        registerPlugin(CorePlugin.INSTANCE);
+        registerPlugin(VanillaPlugin.INSTANCE);
     }
 
     public void registerModPlugins(IRegistrar registrar) {
+        for (IWailaPlugin plugin : corePlugins)
+            registerPluginInRegistrar(registrar, plugin);
         for (IWailaPlugin plugin : plugins)
-            if (plugin.shouldRegister())
-                plugin.register(registrar);
+            registerPluginInRegistrar(registrar, plugin);
+    }
+
+    private void registerPluginInRegistrar(IRegistrar registrar, IWailaPlugin plugin) {
+        if (plugin.shouldRegister() &&
+            !WailaEventRegistrar.postPluginRegister(new WailaRegisterEvent.Plugin(plugin)))
+            plugin.register(registrar);
     }
 
     public void postLoad() {
--- src/client/java/mcp/mobius/waila/utils/NBTUtil.java
+++ src/client/java/mcp/mobius/waila/utils/NBTUtil.java
@@ -5,12 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import net.minecraft.src.CompressedStreamTools;
 import net.minecraft.src.NBTBase;
-import net.minecraft.src.NBTTagByte;
 import net.minecraft.src.NBTTagCompound;
-import net.minecraft.src.NBTTagDouble;
-import net.minecraft.src.NBTTagFloat;
-import net.minecraft.src.NBTTagInt;
-import net.minecraft.src.NBTTagShort;
 
 public final class NBTUtil {
 
@@ -47,15 +42,19 @@ public final class NBTUtil {
 
     public static int getNBTInteger(NBTTagCompound tag, String keyname) {
         NBTBase subtag = tag.func_40196_b(keyname);
-        if (subtag instanceof NBTTagInt)
-            return tag.getInteger(keyname);
-        if (subtag instanceof NBTTagShort)
-            return tag.getShort(keyname);
-        if (subtag instanceof NBTTagByte)
+        if (subtag == null)
+            return 0;
+        if (subtag.getType() == 1)
             return tag.getByte(keyname);
-        if (subtag instanceof NBTTagFloat)
+        if (subtag.getType() == 2)
+            return tag.getShort(keyname);
+        if (subtag.getType() == 3)
+            return tag.getInteger(keyname);
+        if (subtag.getType() == 4)
+            return (int) tag.getLong(keyname);
+        if (subtag.getType() == 5)
             return Math.round(tag.getFloat(keyname));
-        if (subtag instanceof NBTTagDouble)
+        if (subtag.getType() == 6)
             return (int) Math.round(tag.getDouble(keyname));
 
         return 0;
