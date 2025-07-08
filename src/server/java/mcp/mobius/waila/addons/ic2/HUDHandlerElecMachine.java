dc09ef094023dab89e0ce93e06e4c928a573f073
Nico Mexis <nico.mexis@kabelmail.de>
2025-07-08 20:59:50 +0200

    Backport more changes from b1.5_01

--- src/server/java/mcp/mobius/waila/addons/ic2/HUDHandlerElecMachine.java
+++ src/server/java/mcp/mobius/waila/addons/ic2/HUDHandlerElecMachine.java
@@ -7,11 +7,11 @@ import mcp.mobius.waila.utils.WailaExceptionHandler;
 import net.minecraft.src.NBTTagCompound;
 import net.minecraft.src.TileEntity;
 
-public class HUDHandlerElectricMachine implements IDataProvider {
+public class HUDHandlerElecMachine implements IDataProvider {
 
-    public static final IDataProvider INSTANCE = new HUDHandlerElectricMachine();
+    public static final IDataProvider INSTANCE = new HUDHandlerElecMachine();
 
-    private HUDHandlerElectricMachine() {
+    private HUDHandlerElecMachine() {
     }
 
     @Override
