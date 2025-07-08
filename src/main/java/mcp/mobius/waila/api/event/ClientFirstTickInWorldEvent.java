9d8a2c034da2bdce8aec4e080bb1e1069324e597
Nico Mexis <nico.mexis@kabelmail.de>
2025-07-08 20:21:01 +0200

    Backport more changes from 1.4.2

---
+++ src/main/java/mcp/mobius/waila/api/event/ClientFirstTickInWorldEvent.java
@@ -0,0 +1,32 @@
+package mcp.mobius.waila.api.event;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.src.EntityClientPlayerMP;
+import net.minecraft.src.WorldClient;
+import net.minecraftforge.event.Event;
+
+public class ClientFirstTickInWorldEvent extends Event {
+
+    private final Minecraft minecraft;
+    private final WorldClient world;
+    private final EntityClientPlayerMP player;
+
+    public ClientFirstTickInWorldEvent(Minecraft minecraft) {
+        this.minecraft = minecraft;
+        this.world = minecraft.theWorld;
+        this.player = minecraft.thePlayer;
+    }
+
+    public Minecraft getMinecraft() {
+        return minecraft;
+    }
+
+    public WorldClient getWorld() {
+        return world;
+    }
+
+    public EntityClientPlayerMP getPlayer() {
+        return player;
+    }
+
+}
