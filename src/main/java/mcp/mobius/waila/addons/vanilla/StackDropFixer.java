4b9b45bb55c2b89b5e387d6f4022e9b6332f59ea
Nico Mexis <nico.mexis@kabelmail.de>
2025-07-08 20:14:56 +0200

    Backport more changes from 1.4.7

--- src/main/java/mcp/mobius/waila/addons/vanilla/StackDropFixer.java
+++ src/main/java/mcp/mobius/waila/addons/vanilla/StackDropFixer.java
@@ -1,4 +1,4 @@
-package mcp.mobius.waila.addons.ic2;
+package mcp.mobius.waila.addons.vanilla;
 
 import mcp.mobius.waila.api.IDataAccessor;
 import mcp.mobius.waila.api.IDataProvider;
@@ -7,22 +7,36 @@ import mcp.mobius.waila.api.IServerDataAccessor;
 import mcp.mobius.waila.api.ITaggedList;
 import mcp.mobius.waila.utils.ConstantRandom;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 
-public class HUDHandlerDoor implements IDataProvider {
+public class StackDropFixer implements IDataProvider {
 
-    public static final IDataProvider INSTANCE = new HUDHandlerDoor();
+    public static final IDataProvider DEFAULT = new StackDropFixer();
 
-    private HUDHandlerDoor() {
+    private final int metaOverride;
+
+    private StackDropFixer() {
+        this(-1);
+    }
+
+    private StackDropFixer(int metaOverride) {
+        this.metaOverride = metaOverride;
+    }
+
+    public static StackDropFixer withMetaOverride(int metaOverride) {
+        return new StackDropFixer(metaOverride);
     }
 
     @Override
     public ItemStack getStack(IDataAccessor accessor, IPluginConfig config) {
         Block b = accessor.getBlock();
-        int id = b.idDropped(0, ConstantRandom.INSTANCE, 0);
-        return id == 0 ? null : new ItemStack(id, 1, b.damageDropped(0));
+        int meta = metaOverride < 0 ? accessor.getMetadata() : metaOverride;
+        int id = b.idDropped(meta, ConstantRandom.INSTANCE, 0);
+        return id == 0 || Item.itemsList[id] == null
+                ? null : new ItemStack(id, 1, b.damageDropped(meta));
     }
 
     @Override
