a94ca6c4839d526f858904c2497d64b50142e340
Nico Mexis <nico.mexis@kabelmail.de>
2025-07-08 20:12:17 +0200

    Fix new fixers

--- src/main/java/mcp/mobius/waila/addons/vanilla/StackDropFixer.java
+++ src/main/java/mcp/mobius/waila/addons/vanilla/StackDropFixer.java
@@ -7,6 +7,7 @@ import mcp.mobius.waila.api.IServerDataAccessor;
 import mcp.mobius.waila.api.ITaggedList;
 import mcp.mobius.waila.utils.ConstantRandom;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
@@ -34,7 +35,8 @@ public class StackDropFixer implements IDataProvider {
         Block b = accessor.getBlock();
         int meta = metaOverride < 0 ? accessor.getMetadata() : metaOverride;
         int id = b.idDropped(meta, ConstantRandom.INSTANCE, 0);
-        return id == 0 ? null : new ItemStack(id, 1, b.damageDropped(meta));
+        return id == 0 || Item.itemsList[id] == null
+                ? null : new ItemStack(id, 1, b.damageDropped(meta));
     }
 
     @Override
