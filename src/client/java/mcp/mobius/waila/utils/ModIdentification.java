e374973d5df5af6a1a8ca1e100b5d739f9548fb5
Nico Mexis <nico.mexis@kabelmail.de>
2025-07-09 19:34:08 +0200

    Merge changes from upstream

--- src/client/java/mcp/mobius/waila/utils/ModIdentification.java
+++ src/client/java/mcp/mobius/waila/utils/ModIdentification.java
@@ -8,7 +8,6 @@ import java.net.URI;
 import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
@@ -66,7 +65,7 @@ public final class ModIdentification {
 
         try {
             baseModLoop:
-            for (BaseMod mod : (List<BaseMod>) ModLoader.getLoadedMods()) {
+            for (BaseMod mod : ModLoader.getLoadedMods()) {
                 try {
                     String uri = formatURI(mod.getClass().getProtectionDomain().getCodeSource()
                             .getLocation().toURI());
