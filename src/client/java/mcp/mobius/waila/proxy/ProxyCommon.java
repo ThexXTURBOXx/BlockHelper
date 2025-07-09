c41b8fa1a99fdcd7247147317f345fa6e402bb16
Nico Mexis <nico.mexis@kabelmail.de>
2025-07-09 19:56:01 +0200

    Fix core plugins once again

--- src/client/java/mcp/mobius/waila/proxy/ProxyCommon.java
+++ src/client/java/mcp/mobius/waila/proxy/ProxyCommon.java
@@ -36,6 +36,9 @@ public class ProxyCommon {
     }
 
     public void prepare() {
+        registerCorePlugin(CorePlugin.INSTANCE);
+        registerCorePlugin(VanillaPlugin.INSTANCE);
+
         registerPlugin(AdvMachinesASPlugin.INSTANCE);
         registerPlugin(AdvSolarsPlugin.INSTANCE);
         registerPlugin(EEPlugin.INSTANCE);
@@ -48,13 +51,11 @@ public class ProxyCommon {
     }
 
     public void registerCorePlugins(IRegistrar registrar) {
-        registerPlugin(CorePlugin.INSTANCE);
-        registerPlugin(VanillaPlugin.INSTANCE);
+        for (IWailaPlugin plugin : corePlugins)
+            registerPluginInRegistrar(registrar, plugin);
     }
 
     public void registerModPlugins(IRegistrar registrar) {
-        for (IWailaPlugin plugin : corePlugins)
-            registerPluginInRegistrar(registrar, plugin);
         for (IWailaPlugin plugin : plugins)
             registerPluginInRegistrar(registrar, plugin);
     }
--- src/server/java/mcp/mobius/waila/proxy/ProxyCommon.java
+++ src/server/java/mcp/mobius/waila/proxy/ProxyCommon.java
@@ -31,6 +31,9 @@ public class ProxyCommon {
     }
 
     public void prepare() {
+        registerCorePlugin(CorePlugin.INSTANCE);
+        registerCorePlugin(VanillaPlugin.INSTANCE);
+
         registerPlugin(AdvMachinesASPlugin.INSTANCE);
         registerPlugin(AdvSolarsPlugin.INSTANCE);
         registerPlugin(IC2Plugin.INSTANCE);
@@ -38,13 +41,11 @@ public class ProxyCommon {
     }
 
     public void registerCorePlugins(IRegistrar registrar) {
-        registerPlugin(CorePlugin.INSTANCE);
-        registerPlugin(VanillaPlugin.INSTANCE);
+        for (IWailaPlugin plugin : corePlugins)
+            registerPluginInRegistrar(registrar, plugin);
     }
 
     public void registerModPlugins(IRegistrar registrar) {
-        for (IWailaPlugin plugin : corePlugins)
-            registerPluginInRegistrar(registrar, plugin);
         for (IWailaPlugin plugin : plugins)
             registerPluginInRegistrar(registrar, plugin);
     }
