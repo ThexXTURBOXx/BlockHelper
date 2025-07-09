5886711de43273682c8d2d98f544ca3769ffbc4a
Nico Mexis <nico.mexis@kabelmail.de>
2025-07-09 19:56:01 +0200

    Fix core plugins once again

--- src/client/java/mcp/mobius/waila/proxy/ProxyCommon.java
+++ src/client/java/mcp/mobius/waila/proxy/ProxyCommon.java
@@ -34,6 +34,9 @@ public class ProxyCommon {
     }
 
     public void prepare() {
+        registerCorePlugin(CorePlugin.INSTANCE);
+        registerCorePlugin(VanillaPlugin.INSTANCE);
+
         registerPlugin(AdvMachinesASPlugin.INSTANCE);
         registerPlugin(AdvSolarsPlugin.INSTANCE);
         registerPlugin(EEPlugin.INSTANCE);
@@ -44,13 +47,11 @@ public class ProxyCommon {
     }
 
     public void registerCorePlugins(IRegistrar registrar) {
-        registerPlugin(CorePlugin.INSTANCE);
-        registerPlugin(VanillaPlugin.INSTANCE);
+        for (IWailaPlugin plugin : corePlugins)
+            registerPluginInRegistrar(registrar, plugin);
     }
 
     public void registerModPlugins(IRegistrar registrar) {
-        for (IWailaPlugin plugin : corePlugins)
-            registerPluginInRegistrar(registrar, plugin);
         for (IWailaPlugin plugin : plugins)
             registerPluginInRegistrar(registrar, plugin);
     }
