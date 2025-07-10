19954138b8a0bb95e8a8778e6592571ad057b178
Nico Mexis <nico.mexis@kabelmail.de>
2025-07-10 14:42:45 +0200

    Fix EnderChest support
    ... just in case some EnderChest version for this niche MC version exists

--- src/client/java/mcp/mobius/waila/proxy/ProxyCommon.java
+++ src/client/java/mcp/mobius/waila/proxy/ProxyCommon.java
@@ -8,7 +8,7 @@ import mcp.mobius.waila.addons.core.CorePlugin;
 import mcp.mobius.waila.addons.ee.EEPlugin;
 import mcp.mobius.waila.addons.ee2.EE2Plugin;
 import mcp.mobius.waila.addons.ee3.EE3Plugin;
-import mcp.mobius.waila.addons.enderstorage.EnderStoragePlugin;
+import mcp.mobius.waila.addons.enderchest.EnderChestPlugin;
 import mcp.mobius.waila.addons.florasoma.FloraSomaPlugin;
 import mcp.mobius.waila.addons.ic2.IC2Plugin;
 import mcp.mobius.waila.addons.thermalexpansion.ThermalExpansionPlugin;
@@ -44,7 +44,7 @@ public class ProxyCommon {
         registerPlugin(EEPlugin.INSTANCE);
         registerPlugin(EE2Plugin.INSTANCE);
         registerPlugin(EE3Plugin.INSTANCE);
-        registerPlugin(EnderStoragePlugin.INSTANCE);
+        registerPlugin(EnderChestPlugin.INSTANCE);
         registerPlugin(FloraSomaPlugin.INSTANCE);
         registerPlugin(IC2Plugin.INSTANCE);
         registerPlugin(ThermalExpansionPlugin.INSTANCE);
