9cdb6d6ee1ab09a210a1fcbe01dea3c1fa2dc4eb
Nico Mexis <nico.mexis@kabelmail.de>
2025-07-13 12:22:43 +0200

    Backport changes from b1.5_01
    - Made AccessHelper more reliable
    - Fix primed TNT icon
    - Fix a few static imports
    - Add support for matter generator
    - Fix IC² explosives
    - Add missing translations
    - Fix and unify translations
    - Add IndustrialCraft support
    - Remove IC² support

--- src/client/java/mcp/mobius/waila/addons/vanilla/VanillaPlugin.java
+++ src/client/java/mcp/mobius/waila/addons/vanilla/VanillaPlugin.java
@@ -13,6 +13,7 @@ import net.minecraft.src.BlockSign;
 import net.minecraft.src.BlockStep;
 import net.minecraft.src.Entity;
 import net.minecraft.src.EntityLiving;
+import net.minecraft.src.EntityTNTPrimed;
 import net.minecraft.src.ItemRecord;
 import net.minecraft.src.TileEntityFurnace;
 
@@ -58,6 +59,8 @@ public final class VanillaPlugin implements IWailaPlugin {
         registrar.addSyncedConfig("VanillaMC", "vanilla.tnt");
 
         registrar.registerNBTProvider(HUDHandlerEntities.INSTANCE, EntityLiving.class);
+
+        registrar.registerStackProvider(HUDHandlerEntities.INSTANCE, EntityTNTPrimed.class);
 
         registrar.registerBodyProvider(HUDHandlerEntities.INSTANCE, Entity.class);
 
