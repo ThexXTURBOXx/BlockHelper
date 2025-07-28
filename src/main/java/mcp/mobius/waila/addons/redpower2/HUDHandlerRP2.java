5e52af0035bc87c419fc73d9e546f5bb62a3a022
Nico Mexis <nico.mexis@kabelmail.de>
2025-07-28 19:08:46 +0200

    Throw a bit less

--- src/main/java/mcp/mobius/waila/addons/redpower2/HUDHandlerRP2.java
+++ src/main/java/mcp/mobius/waila/addons/redpower2/HUDHandlerRP2.java
@@ -6,6 +6,7 @@ import mcp.mobius.waila.api.IDataProvider;
 import mcp.mobius.waila.api.IPluginConfig;
 import mcp.mobius.waila.api.IServerDataAccessor;
 import mcp.mobius.waila.api.ITaggedList;
+import mcp.mobius.waila.utils.WailaExceptionHandler;
 import net.minecraft.src.Block;
 import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.EnumMovingObjectType;
@@ -104,7 +105,7 @@ public final class HUDHandlerRP2 implements IDataProvider {
                     return is.get(0);
             }
         } catch (Throwable t) {
-            throw new RuntimeException(t);
+            WailaExceptionHandler.handleErr(t, accessor.getTileEntity().getClass(), null);
         }
         return null;
     }
