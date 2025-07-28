eba4a3c38b26423a4425b2b118e327f80acbae14
Nico Mexis <nico.mexis@kabelmail.de>
2025-07-28 19:32:41 +0200

    Throw a bit less

--- src/client/java/mcp/mobius/waila/addons/core/HUDHandlerBlocks.java
+++ src/client/java/mcp/mobius/waila/addons/core/HUDHandlerBlocks.java
@@ -12,6 +12,7 @@ import mcp.mobius.waila.utils.Constants;
 import mcp.mobius.waila.utils.I18n;
 import mcp.mobius.waila.utils.ModIdentification;
 import mcp.mobius.waila.utils.SpawnUtil;
+import mcp.mobius.waila.utils.WailaExceptionHandler;
 import net.minecraft.src.Block;
 import net.minecraft.src.EnumSkyBlock;
 import net.minecraft.src.ItemStack;
@@ -100,7 +101,7 @@ public final class HUDHandlerBlocks implements IDataProvider {
                     currenttip.add(I18n.translate("hud.msg.break_progression") + ": " + progress);
                 }
             } catch (Throwable t) {
-                throw new RuntimeException(t);
+                WailaExceptionHandler.handleErr(t, "curBlockDamageMP", currenttip);
             }
         }
     }
