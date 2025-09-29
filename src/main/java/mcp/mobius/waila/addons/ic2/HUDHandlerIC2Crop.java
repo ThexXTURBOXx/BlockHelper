20f199fdf03247b6bff7438ab3086015a1f2c377
Nico Mexis <nico.mexis@kabelmail.de>
2025-09-29 18:15:00 +0100

    Add IC² crops info

---
+++ src/main/java/mcp/mobius/waila/addons/ic2/HUDHandlerIC2Crop.java
@@ -0,0 +1,88 @@
+package mcp.mobius.waila.addons.ic2;
+
+import java.util.ArrayList;
+import java.util.List;
+import mcp.mobius.waila.addons.core.DefaultCropProvider;
+import mcp.mobius.waila.api.ICropProvider;
+import mcp.mobius.waila.api.IDataAccessor;
+import mcp.mobius.waila.api.IDataProvider;
+import mcp.mobius.waila.api.IPluginConfig;
+import mcp.mobius.waila.api.IServerDataAccessor;
+import mcp.mobius.waila.api.ITaggedList;
+import mcp.mobius.waila.overlay.DisplayUtil;
+import mcp.mobius.waila.utils.I18n;
+import mcp.mobius.waila.utils.WailaExceptionHandler;
+import net.minecraft.src.ItemStack;
+import net.minecraft.src.NBTTagCompound;
+import net.minecraft.src.TileEntity;
+
+import static mcp.mobius.waila.addons.ic2.IC2Plugin.CropCard_canBeHarvested;
+import static mcp.mobius.waila.addons.ic2.IC2Plugin.CropCard_getGain;
+import static mcp.mobius.waila.addons.ic2.IC2Plugin.CropCard_name;
+import static mcp.mobius.waila.addons.ic2.IC2Plugin.TileEntityCrop_crop;
+import static mcp.mobius.waila.addons.ic2.IC2Plugin.TileEntityCrop_id;
+
+public class HUDHandlerIC2Crop extends DefaultCropProvider implements IDataProvider {
+
+    public static final IDataProvider DATA_PROVIDER = new HUDHandlerIC2Crop();
+    public static final ICropProvider CROP_PROVIDER = (ICropProvider) DATA_PROVIDER; // just use same instance...
+
+    private HUDHandlerIC2Crop() {
+        super(0);
+    }
+
+    @Override
+    public ItemStack getStack(IDataAccessor accessor, IPluginConfig config) {
+        return null;
+    }
+
+    @Override
+    public void modifyHead(ItemStack itemStack, ITaggedList<String, String> currenttip,
+                           IDataAccessor accessor, IPluginConfig config) {
+    }
+
+    @Override
+    public void modifyBody(ItemStack itemStack, ITaggedList<String, String> currenttip,
+                           IDataAccessor accessor, IPluginConfig config) {
+        if (config.get("ic2.crop"))
+            try {
+                if (TileEntityCrop_id.getInt(accessor.getTileEntity()) < 0) return;
+                Object card = TileEntityCrop_crop.invoke(accessor.getTileEntity());
+                ItemStack gain = (ItemStack) CropCard_getGain.invoke(card, accessor.getTileEntity());
+                String name = gain == null ? (String) CropCard_name.invoke(card)
+                        : DisplayUtil.itemDisplayNameShortUnformatted(gain);
+                name = name == null || name.isEmpty() ? (String) CropCard_name.invoke(card) : name;
+                currenttip.add(I18n.translate("hud.msg.crop") + ": " + name);
+            } catch (Throwable t) {
+                WailaExceptionHandler.handleErr(t, accessor.getTileEntity().getClass(), currenttip);
+            }
+    }
+
+    @Override
+    public void modifyTail(ItemStack itemStack, ITaggedList<String, String> currenttip,
+                           IDataAccessor accessor, IPluginConfig config) {
+    }
+
+    @Override
+    public void appendServerData(TileEntity te, NBTTagCompound tag,
+                                 IServerDataAccessor accessor, IPluginConfig config) {
+    }
+
+    @Override
+    public List<String> getGrowthDetails(ItemStack itemStack, IDataAccessor accessor, IPluginConfig config) {
+        List<String> ret = new ArrayList<String>();
+        TileEntity te = accessor.getTileEntity();
+        try {
+            if (TileEntityCrop_id.getInt(te) >= 0) {
+                Object card = TileEntityCrop_crop.invoke(te);
+                if ((Boolean) CropCard_canBeHarvested.invoke(card, te)) {
+                    ret.add(getMatureString(itemStack, accessor, config));
+                }
+            }
+        } catch (Throwable t) {
+            WailaExceptionHandler.handleErr(t, te.getClass(), null);
+        }
+        return ret;
+    }
+
+}
