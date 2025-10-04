6f83e6f14cefe10e0ff958e24b1205204e5fe8b4
Nico Mexis <nico.mexis@kabelmail.de>
2025-10-04 14:45:14 +0200

    Show player names

--- src/client/java/mcp/mobius/waila/addons/vanilla/VanillaPlugin.java
+++ src/client/java/mcp/mobius/waila/addons/vanilla/VanillaPlugin.java
@@ -13,6 +13,7 @@ import net.minecraft.src.BlockStem;
 import net.minecraft.src.BlockStep;
 import net.minecraft.src.Entity;
 import net.minecraft.src.EntityLiving;
+import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.EntityTNTPrimed;
 import net.minecraft.src.TileEntityFurnace;
 
@@ -62,6 +63,8 @@ public final class VanillaPlugin implements IWailaPlugin {
         registrar.registerNBTProvider(HUDHandlerEntities.INSTANCE, EntityLiving.class);
 
         registrar.registerStackProvider(HUDHandlerEntities.INSTANCE, EntityTNTPrimed.class);
+
+        registrar.registerHeadProvider(HUDHandlerEntities.INSTANCE, EntityPlayer.class);
 
         registrar.registerBodyProvider(HUDHandlerEntities.INSTANCE, Entity.class);
 
