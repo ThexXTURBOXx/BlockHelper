45e4923e8214f70fa7f8e71baa38c9f78b518bf3
Nico Mexis <nico.mexis@kabelmail.de>
2025-10-04 14:45:14 +0200

    Show player names

--- src/client/java/mcp/mobius/waila/addons/vanilla/HUDHandlerEntities.java
+++ src/client/java/mcp/mobius/waila/addons/vanilla/HUDHandlerEntities.java
@@ -14,6 +14,7 @@ import net.minecraft.src.EntityAgeable;
 import net.minecraft.src.EntityAnimal;
 import net.minecraft.src.EntityLiving;
 import net.minecraft.src.EntityOcelot;
+import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.EntitySheep;
 import net.minecraft.src.EntityTNTPrimed;
 import net.minecraft.src.EntityTameable;
@@ -23,6 +24,7 @@ import net.minecraft.src.EntityZombie;
 import net.minecraft.src.ItemStack;
 import net.minecraft.src.NBTTagCompound;
 
+import static mcp.mobius.waila.api.SpecialChars.WHITE;
 import static mcp.mobius.waila.api.SpecialChars.getRenderString;
 
 public final class HUDHandlerEntities implements IEntityProvider {
@@ -50,6 +52,8 @@ public final class HUDHandlerEntities implements IEntityProvider {
     @Override
     public void modifyHead(Entity entity, ITaggedList<String, String> currenttip,
                            IEntityAccessor accessor, IPluginConfig config) {
+        if (entity instanceof EntityPlayer)
+            currenttip.set(0, WHITE + ((EntityPlayer) entity).username);
     }
 
     @Override
