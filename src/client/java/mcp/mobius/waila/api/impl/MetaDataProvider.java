83e70c2496a3c805005d99b7e1d845a2cf6a2bb8
Nico Mexis <nico.mexis@kabelmail.de>
2025-10-04 15:04:25 +0200

    Port advanced info provider interface from GTNH WAILA
    Introduces unstable interfaces `IAdvDataProvider` and `IAdvEntityProvider`, which may get new functionalities from time to time. I will try to keep them as backward-compatible as possible, though!

--- src/client/java/mcp/mobius/waila/api/impl/MetaDataProvider.java
+++ src/client/java/mcp/mobius/waila/api/impl/MetaDataProvider.java
@@ -3,13 +3,18 @@ package mcp.mobius.waila.api.impl;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import mcp.mobius.waila.api.IAdvDataProvider;
+import mcp.mobius.waila.api.IAdvEntityProvider;
 import mcp.mobius.waila.api.IDataProvider;
 import mcp.mobius.waila.api.IEntityProvider;
 import mcp.mobius.waila.api.ITaggedList;
+import mcp.mobius.waila.api.SpecialChars;
 import mcp.mobius.waila.api.TooltipPosition;
+import mcp.mobius.waila.client.ConfigKeyHandler;
 import mcp.mobius.waila.network.Packet0x01TileRequest;
 import mcp.mobius.waila.network.Packet0x02EntRequest;
 import mcp.mobius.waila.network.WailaPacketHandler;
+import mcp.mobius.waila.utils.I18n;
 import mcp.mobius.waila.utils.WailaExceptionHandler;
 import net.minecraft.src.Block;
 import net.minecraft.src.Entity;
@@ -90,15 +95,37 @@ public class MetaDataProvider {
                     }
             }
 
-        if (tooltipPosition == TooltipPosition.BODY)
+        if (tooltipPosition == TooltipPosition.BODY) {
+            final boolean showAdvancedBody = ConfigKeyHandler.showAdvancedBody;
+            boolean hasAdvancedBodyAvailable = false;
+
             for (List<IDataProvider> providersList : bodyBlockProviders.values()) {
                 for (IDataProvider dataProvider : providersList)
                     try {
                         dataProvider.modifyBody(itemStack, currenttip, accessor, PluginConfig.instance());
+
+                        if (dataProvider instanceof IAdvDataProvider) {
+                            IAdvDataProvider advDataProvider = (IAdvDataProvider) dataProvider;
+                            if (advDataProvider.hasAdvancedBody(itemStack, accessor, PluginConfig.instance())) {
+                                hasAdvancedBodyAvailable = true;
+
+                                if (showAdvancedBody) {
+                                    advDataProvider.modifyAdvancedBody(itemStack, currenttip, accessor,
+                                            PluginConfig.instance());
+                                }
+                            }
+                        }
                     } catch (Throwable t) {
                         WailaExceptionHandler.handleErr(t, dataProvider.getClass(), currenttip);
                     }
             }
+
+            if (hasAdvancedBodyAvailable && !showAdvancedBody) {
+                String keyName = I18n.getKeyDisplayString(ConfigKeyHandler.advancedBodyKey);
+                currenttip.add(SpecialChars.ITALIC + I18n.translate("hud.msg.holdkeymoreinfo", keyName));
+            }
+        }
+
         if (tooltipPosition == TooltipPosition.FOOTER)
             for (List<IDataProvider> providersList : tailBlockProviders.values()) {
                 for (IDataProvider dataProvider : providersList)
@@ -155,15 +182,36 @@ public class MetaDataProvider {
                     }
             }
 
-        if (tooltipPosition == TooltipPosition.BODY)
+        if (tooltipPosition == TooltipPosition.BODY) {
+            final boolean showAdvancedBody = ConfigKeyHandler.showAdvancedBody;
+            boolean hasAdvancedBodyAvailable = false;
+
             for (List<IEntityProvider> providersList : bodyEntityProviders.values()) {
                 for (IEntityProvider dataProvider : providersList)
                     try {
                         dataProvider.modifyBody(entity, currenttip, accessor, PluginConfig.instance());
+
+                        if (dataProvider instanceof IAdvEntityProvider) {
+                            IAdvEntityProvider advDataProvider = (IAdvEntityProvider) dataProvider;
+                            if (advDataProvider.hasAdvancedBody(entity, accessor, PluginConfig.instance())) {
+                                hasAdvancedBodyAvailable = true;
+
+                                if (showAdvancedBody) {
+                                    advDataProvider.modifyAdvancedBody(entity, currenttip, accessor,
+                                            PluginConfig.instance());
+                                }
+                            }
+                        }
                     } catch (Throwable t) {
                         WailaExceptionHandler.handleErr(t, dataProvider.getClass(), currenttip);
                     }
             }
+
+            if (hasAdvancedBodyAvailable && !showAdvancedBody) {
+                String keyName = I18n.getKeyDisplayString(ConfigKeyHandler.advancedBodyKey);
+                currenttip.add(SpecialChars.ITALIC + I18n.translate("hud.msg.holdkeymoreinfo", keyName));
+            }
+        }
 
         if (tooltipPosition == TooltipPosition.FOOTER)
             for (List<IEntityProvider> providersList : tailEntityProviders.values()) {
