5d348b45c55840a128d1500ad25ddfcbf8e31a27
Nico Mexis <nico.mexis@kabelmail.de>
2026-03-15 14:42:46 +0100

    Use static imports in plugins

--- src/server/java/mcp/mobius/waila/addons/bc2/HUDHandlerBC2Energy.java
+++ src/server/java/mcp/mobius/waila/addons/bc2/HUDHandlerBC2Energy.java
@@ -7,6 +7,15 @@ import mcp.mobius.waila.utils.WailaExceptionHandler;
 import net.minecraft.src.NBTTagCompound;
 import net.minecraft.src.TileEntity;
 
+import static mcp.mobius.waila.addons.bc2.BC2Plugin.Engine_energy;
+import static mcp.mobius.waila.addons.bc2.BC2Plugin.Engine_maxEnergy;
+import static mcp.mobius.waila.addons.bc2.BC2Plugin.IPowerReceptor;
+import static mcp.mobius.waila.addons.bc2.BC2Plugin.IPowerReceptor_getPowerProvider;
+import static mcp.mobius.waila.addons.bc2.BC2Plugin.PowerProvider_energyStored;
+import static mcp.mobius.waila.addons.bc2.BC2Plugin.PowerProvider_maxEnergyStored;
+import static mcp.mobius.waila.addons.bc2.BC2Plugin.TileEngine;
+import static mcp.mobius.waila.addons.bc2.BC2Plugin.TileEngine_engine;
+
 public final class HUDHandlerBC2Energy implements IDataProvider {
 
     public static final IDataProvider INSTANCE = new HUDHandlerBC2Energy();
@@ -20,17 +29,17 @@ public final class HUDHandlerBC2Energy implements IDataProvider {
         try {
             int energy = -1;
             int maxsto = -1;
-            if (BC2Plugin.TileEngine.isInstance(te)) {
-                Object engine = BC2Plugin.TileEngine_engine.get(te);
+            if (TileEngine.isInstance(te)) {
+                Object engine = TileEngine_engine.get(te);
                 if (engine != null) {
-                    energy = BC2Plugin.Engine_energy.getInt(engine);
-                    maxsto = BC2Plugin.Engine_maxEnergy.getInt(engine);
+                    energy = Engine_energy.getInt(engine);
+                    maxsto = Engine_maxEnergy.getInt(engine);
                 }
-            } else if (BC2Plugin.IPowerReceptor.isInstance(te)) {
-                Object prov = BC2Plugin.IPowerReceptor_getPowerProvider.invoke(te);
+            } else if (IPowerReceptor.isInstance(te)) {
+                Object prov = IPowerReceptor_getPowerProvider.invoke(te);
                 if (prov != null) {
-                    energy = BC2Plugin.PowerProvider_energyStored.getInt(prov);
-                    maxsto = BC2Plugin.PowerProvider_maxEnergyStored.getInt(prov);
+                    energy = PowerProvider_energyStored.getInt(prov);
+                    maxsto = PowerProvider_maxEnergyStored.getInt(prov);
                 }
             }
 
