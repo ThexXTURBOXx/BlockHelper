0ab386ad2dbd775dd4950b9ee45b34360ebfaf37
Nico Mexis <nico.mexis@kabelmail.de>
2026-04-24 18:28:07 +0200

    Add chicken next egg timer

--- src/client/java/mcp/mobius/waila/addons/vanilla/VanillaPlugin.java
+++ src/client/java/mcp/mobius/waila/addons/vanilla/VanillaPlugin.java
@@ -47,6 +47,7 @@ public final class VanillaPlugin implements IWailaPlugin {
         registrar.addSyncedConfig("VanillaMC", "vanilla.showhp");
         registrar.addSyncedConfig("VanillaMC", "vanilla.tame");
         registrar.addSyncedConfig("VanillaMC", "vanilla.sheep");
+        registrar.addSyncedConfig("VanillaMC", "vanilla.chicken");
         registrar.addSyncedConfig("VanillaMC", "vanilla.tnt");
 
         registrar.registerNBTProvider(HUDHandlerEntities.INSTANCE, EntityLiving.class);
