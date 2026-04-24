a5ef02ae1427f014959f449974382f530a81f51f
Nico Mexis <nico.mexis@kabelmail.de>
2026-04-24 22:44:39 +0200

    Improve "cheat" infos

--- src/client/java/mcp/mobius/waila/addons/vanilla/VanillaPlugin.java
+++ src/client/java/mcp/mobius/waila/addons/vanilla/VanillaPlugin.java
@@ -90,6 +90,7 @@ public final class VanillaPlugin implements IWailaPlugin {
         registrar.registerCropProvider(new DefaultCropProvider(7), BlockStem.class);
         registrar.registerCropProvider(new DefaultCropProvider(3), BlockNetherStalk.class);
 
+        registrar.addSyncedConfig("VanillaMC", "vanilla.silverfish");
         registrar.addSyncedConfig("VanillaMC", "vanilla.jukebox");
         registrar.addSyncedConfig("VanillaMC", "vanilla.noteblock");
 
@@ -101,7 +102,6 @@ public final class VanillaPlugin implements IWailaPlugin {
         registrar.addConfig("VanillaMC", "vanilla.leverstate");
         registrar.addConfig("VanillaMC", "vanilla.repeater");
         registrar.addConfig("VanillaMC", "vanilla.redstone");
-        registrar.addConfig("VanillaMC", "vanilla.silverfish");
 
         registrar.registerStackProvider(HUDHandlerVanilla.INSTANCE, silverfish.getClass());
         registrar.registerStackProvider(HUDHandlerVanilla.INSTANCE, redstone.getClass());
