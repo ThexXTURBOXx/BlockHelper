5c713a22146a2ac5d64ab9a223aeb25f283342ca
Nico Mexis <nico.mexis@kabelmail.de>
2026-04-26 13:30:13 +0200

    Fix a few tags and improve handling

--- src/client/java/mcp/mobius/waila/addons/vanilla/HUDHandlerVanilla.java
+++ src/client/java/mcp/mobius/waila/addons/vanilla/HUDHandlerVanilla.java
@@ -76,7 +76,7 @@ public final class HUDHandlerVanilla implements IDataProvider {
         /* Mob spawner handler */
         if (block == mobSpawner && accessor.getTileEntity() instanceof TileEntityMobSpawner
             && config.get("vanilla.spawntype")) {
-            String name = currenttip.get(0);
+            String name = currenttip.getFirstEntry(HUDHandlerBlocks.BLOCK_NAME_TAG);
             String mobname = ((TileEntityMobSpawner) accessor.getTileEntity()).entityID;
             currenttip.replaceFirstTagEntry(name + " (" + mobname + ")", HUDHandlerBlocks.BLOCK_NAME_TAG);
         }
