d4e96182c8611eb49a002d8cab5f1b462191e690
Nico Mexis <nico.mexis@kabelmail.de>
2026-05-19 22:29:16 +0200

    Backport 1.0 changes
    - Add reference to a1.2.6 release
    - Fix config buttons
    - Better repeater and comparator overlay labels and info
    - Proper RedPower² support
    - Port newest changes from GTNH WAILA (fix overflows and packet sizes)
    - Add NBT print function

---
+++ src/client/java/mcp/mobius/waila/addons/redpower2/HUDHandlerGateLogic.java
@@ -0,0 +1,76 @@
+package mcp.mobius.waila.addons.redpower2;
+
+import mcp.mobius.waila.api.IDataAccessor;
+import mcp.mobius.waila.api.IDataProvider;
+import mcp.mobius.waila.api.IPluginConfig;
+import mcp.mobius.waila.api.IServerDataAccessor;
+import mcp.mobius.waila.api.ITaggedList;
+import mcp.mobius.waila.utils.I18n;
+import net.minecraft.src.ItemStack;
+import net.minecraft.src.NBTTagCompound;
+import net.minecraft.src.TileEntity;
+
+import static mcp.mobius.waila.api.SpecialChars.TAB;
+
+public final class HUDHandlerGateLogic implements IDataProvider {
+
+    public static final IDataProvider INSTANCE = new HUDHandlerGateLogic();
+
+    static final int[] REPEATER_DELAYS = new int[]{1, 2, 3, 4, 8, 16, 32, 64, 128};
+
+    private HUDHandlerGateLogic() {
+    }
+
+    @Override
+    public ItemStack getStack(IDataAccessor accessor, IPluginConfig config) {
+        return null;
+    }
+
+    @Override
+    public void modifyHead(ItemStack itemStack, ITaggedList<String, String> currenttip,
+                           IDataAccessor accessor, IPluginConfig config) {
+    }
+
+    @Override
+    public void modifyBody(ItemStack itemStack, ITaggedList<String, String> currenttip,
+                           IDataAccessor accessor, IPluginConfig config) {
+        if (!config.get("pr.showdata")) return;
+
+        String type = accessor.getNBTData().getString("id");
+        if (type == null) return;
+
+        if (type.equals("IRRepeater")) {
+            int tick = REPEATER_DELAYS[accessor.getNBTInteger("dm")];
+            if (tick == 1)
+                currenttip.add(I18n.translate("hud.msg.delay") + ": " +
+                               I18n.translate("hud.msg.tick"));
+            else
+                currenttip.add(I18n.translate("hud.msg.delay") + ": " +
+                               I18n.translate("hud.msg.ticks_format", tick));
+        } else if (type.equals("Timer") || type.equals("Sequencer")) {
+            int intv = accessor.getNBTInteger("iv");
+            if (type.equals("Timer")) intv += 2;
+            String pmax = String.format("%.2f", intv / 20f);
+            currenttip.add(I18n.translate("hud.msg.delay") + ": " +
+                           I18n.translate("hud.msg.seconds_format", pmax));
+        } else if (type.equals("IRCounter")) {
+            int val = accessor.getNBTInteger("cnt");
+            int max = accessor.getNBTInteger("max");
+            int dec = accessor.getNBTInteger("dec");
+            int inc = accessor.getNBTInteger("inc");
+            currenttip.add(I18n.translate("hud.msg.value") + ":" + TAB + val + " / " + max);
+            currenttip.add(I18n.translate("hud.msg.step") + ":" + TAB + "-" + dec + " / " + "+" + inc);
+        }
+    }
+
+    @Override
+    public void modifyTail(ItemStack itemStack, ITaggedList<String, String> currenttip,
+                           IDataAccessor accessor, IPluginConfig config) {
+    }
+
+    @Override
+    public void appendServerData(TileEntity te, NBTTagCompound tag,
+                                 IServerDataAccessor accessor, IPluginConfig config) {
+    }
+
+}
--- src/client/java/mcp/mobius/waila/utils/NBTUtil.java
+++ src/client/java/mcp/mobius/waila/utils/NBTUtil.java
@@ -15,43 +15,43 @@ public final class NBTUtil {
 
     private static final Field tagMap;
 
-    private NBTUtil() {
-        throw new UnsupportedOperationException();
-    }
-
     static {
         try {
-            tagMap = AccessHelper.getDeclaredField(NBTTagCompound.class, "a", "field_1094_a", "tagMap");
+            tagMap = AccessHelper.getDeclaredField(NBTTagCompound.class, "tagMap", "field_1094_a", "a");
         } catch (Throwable t) {
             throw new RuntimeException(t);
         }
     }
 
-    public static void writeNBTTagCompound(NBTTagCompound par0NBTTagCompound, DataOutputStream par1DataOutputStream) throws IOException {
-        if (par0NBTTagCompound == null) {
-            par1DataOutputStream.writeShort(-1);
+    private NBTUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void writeNBTTagCompound(NBTTagCompound nbt, DataOutputStream target) throws IOException {
+        if (nbt == null) {
+            target.writeInt(-1);
         } else {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            CompressedStreamTools.writeGzippedCompoundToOutputStream(par0NBTTagCompound, baos);
+            CompressedStreamTools.writeGzippedCompoundToOutputStream(nbt, baos);
             byte[] abyte = baos.toByteArray();
 
             if (abyte.length > 32000)
-                par1DataOutputStream.writeShort(-1);
+                target.writeInt(-1);
             else {
-                par1DataOutputStream.writeShort((short) abyte.length);
-                par1DataOutputStream.write(abyte);
+                target.writeInt(abyte.length);
+                target.write(abyte);
             }
         }
     }
 
-    public static NBTTagCompound readNBTTagCompound(DataInputStream par0DataInputStream) throws IOException {
-        short short1 = par0DataInputStream.readShort();
+    public static NBTTagCompound readNBTTagCompound(DataInputStream dat) throws IOException {
+        int val = dat.readInt();
 
-        if (short1 < 0) {
+        if (val < 0) {
             return null;
         } else {
-            byte[] abyte = new byte[short1];
-            par0DataInputStream.readFully(abyte);
+            byte[] abyte = new byte[val];
+            dat.readFully(abyte);
             return CompressedStreamTools.loadGzippedCompoundFromOutputStream(new ByteArrayInputStream(abyte));
         }
     }
@@ -78,6 +78,23 @@ public final class NBTUtil {
         }
 
         return 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static String toString(NBTBase nbt) {
+        try {
+            if (nbt instanceof NBTTagCompound) {
+                NBTTagCompound tag = (NBTTagCompound) nbt;
+                StringBuilder sb = new StringBuilder(tag.getKey() + ":[");
+                for (Map.Entry<String, NBTBase> e : ((Map<String, NBTBase>) tagMap.get(tag)).entrySet()) {
+                    sb.append(e.getKey()).append(":").append(toString(e.getValue())).append(",");
+                }
+                return sb + "]";
+            }
+        } catch (Throwable t) {
+            WailaExceptionHandler.handleErr(t, "NBTUtil#toString", null);
+        }
+        return nbt.toString();
     }
 
 }
