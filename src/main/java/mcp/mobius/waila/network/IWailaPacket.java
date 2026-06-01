130287094958c6c868f52783cd96109f6f47c3dd
Nico Mexis <nico.mexis@kabelmail.de>
2026-06-01 20:19:08 +0200

    Backport changes
    - Fix translation key in Czechian
    - Remove cruft
    - Harden packet logic a bit
    - Fix liquid bar rendering with mipmaps enabled

--- src/main/java/mcp/mobius/waila/network/IWailaPacket.java
+++ src/main/java/mcp/mobius/waila/network/IWailaPacket.java
@@ -6,6 +6,8 @@ import java.io.DataOutputStream;
 
 public interface IWailaPacket {
 
+    int MAX_REACH_SQ = 36;
+
     void encode(DataOutputStream target) throws Exception;
 
     void decode(DataInputStream dat);
