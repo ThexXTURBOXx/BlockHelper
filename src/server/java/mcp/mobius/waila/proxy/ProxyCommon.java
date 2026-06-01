acb11a69272e712901939922dd317dbcdec10b40
Nico Mexis <nico.mexis@kabelmail.de>
2026-06-01 21:57:43 +0200

    Backport changes
    - Fix translation key in Czechian
    - Remove cruft
    - Harden packet logic a bit
    - Fix liquid bar rendering with mipmaps enabled
    - Remove dedicated Advanced Machines support (already handled by IC²)

--- src/server/java/mcp/mobius/waila/proxy/ProxyCommon.java
+++ src/server/java/mcp/mobius/waila/proxy/ProxyCommon.java
@@ -2,7 +2,6 @@ package mcp.mobius.waila.proxy;
 
 import java.util.ArrayList;
 import java.util.List;
-import mcp.mobius.waila.addons.advmachines.as.AdvMachinesASPlugin;
 import mcp.mobius.waila.addons.advsolars.AdvSolarsPlugin;
 import mcp.mobius.waila.addons.bc2.BC2Plugin;
 import mcp.mobius.waila.addons.core.CorePlugin;
@@ -36,7 +35,6 @@ public class ProxyCommon {
         registerCorePlugin(CorePlugin.INSTANCE);
         registerCorePlugin(VanillaPlugin.INSTANCE);
 
-        registerPlugin(AdvMachinesASPlugin.INSTANCE);
         registerPlugin(AdvSolarsPlugin.INSTANCE);
         registerPlugin(ICPlugin.INSTANCE);
         registerPlugin(IC2Plugin.INSTANCE);
